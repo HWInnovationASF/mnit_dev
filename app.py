8283d85b65721c81f1fa5df505e977fc8e4342b2
HWInnovationASF <hwinnoasefa@gmail.com>
2026-04-08 19:56:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -434,7 +434,7 @@ def handle_ice(data):
 
 if __name__ == '__main__':
 
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
 
 
